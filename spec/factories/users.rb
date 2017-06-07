32107050d68191c7584108eefcbcd1caf26560c8
EvanYang <yangyi2048@outlook.com>
2017-06-07 19:14:34 +0800

    add capybara and database_cleaner to test features

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password 'secret'
-    password_confirmation 'secret'
+    password_confirmation { password }
 
     factory :admin do
       admin true
